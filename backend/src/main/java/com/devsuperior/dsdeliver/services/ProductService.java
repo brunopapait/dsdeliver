e43dfaca79014900059f43fe0d8b4ed4d16739a4
brunopapait <brunohpapait@gmail.com>
2021-01-07 21:27:37 -0300

    Get /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll() throws Exception {
+		List<Product> products = productRepository.findAllByOrderByNameAsc();
+		
+		if (products.isEmpty()) {
+			throw new Exception("A lista de produtos estão vazias.");
+		}
+		
+		return products.stream().map(product -> new ProductDTO(product)).collect(Collectors.toList());
+
+	}
+}
